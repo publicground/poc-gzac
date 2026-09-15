bdbffb36483587c4d72a6f3366d0beebf728ec5e
Test-Automation-Engineer <menno.slingerland@denhaag.nl>
2026-09-15 14:12:00 +0200

    fix: relative config dir for job

--- apps/openzaak/additionalmanifests/local/permissies/set-permissions.py
+++ apps/openzaak/additionalmanifests/local/permissies/set-permissions.py
@@ -11,7 +11,14 @@ import requests
 secret_key = os.environ.get('OPENZAAK_SECRET_KEY', 'dummy')
 openzaak_url = os.environ.get('OPENZAAK_URL', 'http://openzaak.wigo4it.nl')
 
-CONFIG_DIR = os.environ.get('CONFIG_DIR', './config')
+# Bepaal het absolute pad van de map waarin DIT script (set-permissions.py) staat (bijv. /scripts)
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Als CONFIG_DIR niet als environment variable is meegegeven, 
+# gebruik dan automatisch de 'config' submap náást dit script (bijv. /scripts/config)
+DEFAULT_CONFIG_DIR = os.path.join(SCRIPT_DIR, 'config')
+CONFIG_DIR = os.environ.get('CONFIG_DIR', DEFAULT_CONFIG_DIR)
+
 search_path = os.path.join(CONFIG_DIR, '*.json')
 
 # ------------------------------------------------------------------------------
